7b088b77723c9b7dc55c29d12705f41e47186919
Brian John Luk <brian@cloudinary.com>
2019-08-13 11:16:32 -0700

    added --from_url option for config

--- cloudinary_cli/core/config.py
+++ cloudinary_cli/core/config.py
@@ -10,8 +10,9 @@ from click import command, argument, option, Choice
 eg. cld config -n <NAME> <CLOUDINARY_URL>""", nargs=2)
 @option("-ls", "--ls", help="List all configurations", is_flag=True)
 @option("-rm", "--rm", help="Delete an additional configuration", nargs=1)
-def config(new, ls, rm):
-    if not (new or ls or rm):
+@option("-url", "--from_url", help="Create a configuration from a Cloudinary URL", nargs=1)
+def config(new, ls, rm, from_url):
+    if not (new or ls or rm or from_url):
         print('\n'.join(["{}:\t{}".format(k, v if k != "api_secret" else "***************{}".format(v[-4:])) for k, v in cloudinary._config.__dict__.items()]))
         exit(0)
 
@@ -43,3 +44,20 @@ def config(new, ls, rm):
         del cfg[rm]
         open(CLOUDINARY_CLI_CONFIG_FILE, "w").write(dumps(cfg))
         print("Configuration '{}' deleted".format(rm))
+        exit(0)
+    if from_url:
+        if "CLOUDINARY_URL=" in from_url:
+            from_url = from_url[15:]
+        try:
+            cloudinary._config._parse_cloudinary_url(from_url)
+            cfg[cloudinary._config.cloud_name] = from_url
+            api.ping()
+            with open(CLOUDINARY_CLI_CONFIG_FILE, "w") as f:
+                f.write(dumps(cfg))
+                f.close()
+            print("Config '{}' saved!".format(from_url))
+        except Exception as e:
+            print(e)
+            print("Invalid Cloudinary URL: {}".format(from_url))
+            exit(1)
+        exit(0)
