01e64c9abe54ad13b17aa3b18bc3a58dab91c84c
Brian John Luk <brian@cloudinary.com>
2019-08-13 11:17:42 -0700

    Added override description

--- cloudinary_cli/core/overrides.py
+++ cloudinary_cli/core/overrides.py
@@ -1,32 +1,22 @@
 # from ..utils import *
-from webbrowser import open as open_url
-from csv import DictWriter
 from cloudinary import api, uploader
 
 import click
-from click import Command
 from click.utils import make_str
 from click.parser import split_opt
 
+
+# overrides click.MultiCommand.resolve_command
 def resolve_command(self, ctx, args):
     cmd_name = make_str(args[0])
     original_cmd_name = cmd_name
 
-    # Get the command
     cmd = self.get_command(ctx, cmd_name)
 
-    # If we can't find the command but there is a normalization
-    # function available, we try with that one.
     if cmd is None and ctx.token_normalize_func is not None:
         cmd_name = ctx.token_normalize_func(cmd_name)
         cmd = self.get_command(ctx, cmd_name)
 
-    # If we don't find the command we want to show an error message
-    # to the user that it was not provided.  However, there is
-    # something else we should do: if the first argument looks like
-    # an option we want to kick off parsing again for arguments to
-    # resolve things like --help which now should go to the main
-    # place.
     if cmd is None and not ctx.resilient_parsing:
         if split_opt(cmd_name)[0]:
             self.parse_args(ctx, ctx.args)
