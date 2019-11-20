8879fd68d644a689255f23153627a75e61144783
Brian Luk <brian@cloudinary.com>
2019-11-20 13:41:57 -0800

    Use `cloudinary.USER_PLATFORM` instead of modifying `cloudinary.USER_AGENT`

--- cloudinary_cli/__init__.py
+++ cloudinary_cli/__init__.py
@@ -1,11 +1,5 @@
-from sys import version_info
-
 import cloudinary
 
 __version__ = "0.4.0b"
 
-cloudinary.USER_PLATFORM
-cloudinary.USER_AGENT = "CloudinaryCLI/{} (Python {}, pycloudinary {})".format(
-    __version__,
-    ".".join(map(str, version_info[0:3])),
-    cloudinary.VERSION)
+cloudinary.USER_PLATFORM = "CloudinaryCLI/{}".format(__version__)
