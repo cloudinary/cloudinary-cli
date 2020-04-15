2fdf2f3c1e264f38ca110620105dd883d2b44f4d
Constantine Nathanson <constantine@cloudinary.com>
2020-04-15 16:43:47 +0300

    Fix unit tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,18 +1,20 @@
+import unittest
+
 from cloudinary_cli.utils.utils import parse_option_value
 
 
-def test_parse_option_value():
-    """ should parse option values correctly """
-    assert parse_option_value("haha,123,1234") == "haha,123,1234"
-    assert parse_option_value("True") is True
-    assert parse_option_value("false") is False
-    assert parse_option_value('["test","123"]') == ['test', '123']
-    assert parse_option_value('{"foo":"bar"}') == {"foo": "bar"}
-    assert parse_option_value('{"an":"object","or":"dict"}') == {"an": "object", "or": "dict"}
-    assert parse_option_value('["this","will","be","read","as","a","list"]') == ["this", "will", "be", "read", "as",
-                                                                                 "a", "list"]
+class UtilsTest(unittest.TestCase):
+    def test_parse_option_value(self):
+        """ should parse option values correctly """
+        self.assertEqual("haha,123,1234", parse_option_value("haha,123,1234"))
+        self.assertTrue(parse_option_value("True"))
+        self.assertFalse(parse_option_value("false"))
+        self.assertListEqual(["test", "123"], parse_option_value('["test","123"]'))
+        self.assertDictEqual({"foo": "bar"}, parse_option_value('{"foo":"bar"}'))
+        self.assertDictEqual({"an": "object", "or": "dict"}, parse_option_value('{"an":"object","or":"dict"}'))
+        self.assertListEqual(["this", "will", "be", "read", "as",
+                              "a", "list"], parse_option_value('["this","will","be","read","as","a","list"]'))
 
-
-def test_parse_option_value_converts_int_to_str():
-    """ should convert a parsed int to a str """
-    assert parse_option_value(1) == "1"
+    def test_parse_option_value_converts_int_to_str(self):
+        """ should convert a parsed int to a str """
+        self.assertEqual("1", parse_option_value(1))
