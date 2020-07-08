677f30e60b912470abfc5ae85076f2a4d1f6b717
Brian Luk <brian@cloudinary.com>
2020-07-08 15:10:29 -0700

    Refactor path/public_id formatting

--- cloudinary_cli/utils/file_utils.py
+++ cloudinary_cli/utils/file_utils.py
@@ -1,4 +1,5 @@
-from os import walk, path, listdir, rmdir
+from os import walk, path, listdir, rmdir, sep
+from os.path import split, relpath, abspath
 
 from cloudinary_cli.defaults import logger
 from cloudinary_cli.utils.utils import etag
@@ -30,3 +31,22 @@ def delete_empty_dirs(root, remove_root=False):
     if len(files) == 0 and remove_root:
         logger.debug(f"Removing empty folder '{root}'")
         rmdir(root)
+
+
+def get_destination_folder(cloudinary_folder: str, file_path: str, parent: str = None) -> str:
+    """
+    :param cloudinary_folder:   Destination folder in Cloudinary
+    :param file_path:           Path to the local file
+    :param parent:              Parent folder of the directory to upload/sync
+    :return:                    Value passed to `folder` when uploading to Cloudinary
+    """
+    folder_path = []
+
+    parent_path = abspath(parent) if parent else None
+    splitted = split(relpath(file_path, parent_path))
+
+    if splitted[0]:
+        folder_path = splitted[0].split(sep)
+
+    return "/".join([cloudinary_folder, *folder_path]).strip("/")
+
