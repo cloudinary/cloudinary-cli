6d036cd1d498fc954a0eaea86f7b752166510c3b
Brian Luk <brian@cloudinary.com>
2020-07-21 11:48:42 -0700

    Simplify auto pagination usage

--- cloudinary_cli/core/admin.py
+++ cloudinary_cli/core/admin.py
@@ -19,22 +19,19 @@ Format: cld <cli options> admin <command options> <method> <method parameters>
 @option("-o", "--optional_parameter", multiple=True, nargs=2, help="Pass optional parameters as raw strings.")
 @option("-O", "--optional_parameter_parsed", multiple=True, nargs=2,
         help="Pass optional parameters as interpreted strings.")
-@option("-A", "--auto_paginate_field", nargs=1, help="Return all results in the specified field. \
-        Will call Admin API multiple times using the value passed in --cursor_field.", default=None)
-@option("-c", "--cursor_field", nargs=1, help="Cursor field to use when using auto paginate.\
-        Default is next_cursor - use derived_next_cursor when using the resource method.", default='next_cursor')
+@option("-A", "--auto_paginate", is_flag=True,
+        help="Will auto paginate Admin API using the value passed in --cursor_field.", default=False)
 @option("-ff", "--filter_fields", multiple=True, help="Filter fields to return when using auto pagination.")
 @option("-ls", "--ls", is_flag=True, help="List all available methods in the Admin API.")
 @option("--save", nargs=1, help="Save output to a file.")
 @option("-d", "--doc", is_flag=True, help="Open the Admin API reference in a browser.")
 def admin(params, optional_parameter, optional_parameter_parsed, 
-          auto_paginate_field, cursor_field, filter_fields, 
+          auto_paginate, filter_fields,
           ls, save, doc):
     return handle_api_command(params, optional_parameter, optional_parameter_parsed,
                               ls, save, doc,
                               doc_url="https://cloudinary.com/documentation/admin_api",
                               api_instance=api,
                               api_name="admin",
-                              auto_paginate_field=auto_paginate_field,
-                              cursor_field=cursor_field,
+                              auto_paginate=auto_paginate,
                               filter_fields=filter_fields)
