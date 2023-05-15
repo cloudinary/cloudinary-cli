b79a1be8246465f121cb6b26ff93f8ced632c67e
Constantine Nathanson <constantine@cloudinary.com>
2023-05-15 17:25:43 +0300

    Bump `pycloudinary` to `1.33.0`

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ with open("README.md", "r") as rmf:
 with open('cloudinary_cli/version.py') as vf:
     version = vf.readline().strip().split('"')[1]
 
+with open('requirements.txt') as rqf:
+    requirements = rqf.read().splitlines()
+
 setuptools.setup(
     name="cloudinary-cli",
     version=version,
@@ -35,15 +38,7 @@ setuptools.setup(
     python_requires='>=3.6.0',
     setup_requires=["pytest-runner"],
     tests_require=["pytest", "mock", "urllib3"],
-    install_requires=[
-        "cloudinary>=1.28.1",
-        "pygments",
-        "jinja2",
-        "click",
-        "click-log",
-        "requests",
-        "docstring-parser"
-    ],
+    install_requires=requirements,
     include_package_data=True,
     zip_safe=False
 )
