8eb3045d2d2b3eb4d8be5835da6fb8fe7c8849bb
Gagandeep Pratihar <gagandeep.pratihar@landisgyr.com>
2024-10-15 20:14:37 +0530

    Stubs added

---
+++ cloudinary_cli/core/overrides.pyi
@@ -0,0 +1,18 @@
+# cloudinary_cli/stubs/overrides.pyi
+
+from typing import Any, Dict, Tuple, Optional, Callable
+from click import Context, Command
+from cloudinary.uploader import UploadResponse
+from cloudinary.utils import CloudinaryUrlResponse
+
+# Define a type for the `resolve_command` method
+def resolve_command(self: Any, ctx: Context, args: list[str]) -> Tuple[str, Optional[Command], list[str]]:
+    ...
+
+# Define a type for the `upload` function
+def upload(file: Any, **options: Any) -> UploadResponse: 
+    ...
+
+# Define a type for the `cloudinary_url` function
+def cloudinary_url(source: Any, **options: Any) -> CloudinaryUrlResponse:
+    ...
