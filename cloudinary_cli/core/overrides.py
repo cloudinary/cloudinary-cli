3810307c5a5c27b39b185ec151631ebbb12b9e08
Constantine Nathanson <35217733+const-cloudinary@users.noreply.github.com>
2025-01-20 12:01:57 +0200

    Improve doc strings

--- cloudinary_cli/core/overrides.py
+++ cloudinary_cli/core/overrides.py
@@ -35,6 +35,24 @@ def resolve_command(self, ctx, args):
 
 # Patch to set `auto` resource type
 def upload(file, **options):
+    """
+    Uploads an asset to a Cloudinary cloud.
+
+    The asset can be:
+       * a local file path
+       * the actual data (byte array buffer)
+       * the Data URI (Base64 encoded), max ~60 MB (62,910,000 chars)
+       * the remote FTP, HTTP or HTTPS URL address of an existing file
+       * a private storage bucket (S3 or Google Storage) URL of a whitelisted bucket
+
+    See: https://cloudinary.com/documentation/image_upload_api_reference#upload_method
+    :param file: The asset to upload.
+    :type file: Any or str
+    :param options: The optional parameters. See the upload API documentation.
+    :type options: dict, optional
+    :return: The result of the upload API call.
+    :rtype: dict
+    """
     if "resource_type" not in options.keys():
         options["resource_type"] = "auto"
     return original_upload(file, **options)
